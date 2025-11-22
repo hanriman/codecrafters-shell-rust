b9b9352bad44504713a77b6a026fe892574f61bf
Hanifan Rizki Nurahman <hanifanrizki@gmail.com>
2025-11-22 12:12:59 +0100

    implement echo

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+enum Command {
+    Exit,
+    Echo { display_string: String },
+    NotFound,
+}
+
+impl Command {
+    fn from_input(input: &str) -> Self {
+        let input = input.trim();
+        if input == "exit" {
+            return Self::Exit;
+        }
+        if let Some(idx) = input.find("echo ") {
+            if idx == 0 {
+                return Self::Echo {
+                    display_string: input["echo ".len()..].to_string(),
+                };
+            }
+        }
+        Self::NotFound
+    }
+}
+
 fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         // wait for user input
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let command = Command::from_input(&input);
 
-        match command.trim() {
-            "exit" => std::process::exit(0),
-            _ => println!("{}: command not found", command.trim()),
+        match command {
+            Command::Exit => std::process::exit(0),
+            Command::Echo { display_string } => println!("{}", display_string),
+            Command::NotFound => println!("{}: command not found", input.trim()),
         }
     }
 }
