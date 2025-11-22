3e4fcf10703444dd6bd9fd53a7fa632aa246e1bd
Hanifan Rizki Nurahman <hanifanrizki@gmail.com>
2025-11-22 11:03:19 +0100

    basic stdin and stdout

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // wait for user input
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap_or(404);
+    println!("{}: command not found", command.trim());
 }
