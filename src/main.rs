f4af90321804f4955b340d4e067ad2884079d372
Hanifan Rizki Nurahman <hanifanrizki@gmail.com>
2025-11-23 14:34:32 +0100

    update structure and implement type

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+const BUILT_IN_COMMANDS: [&str; 3] = ["exit", "echo", "type"];
+
 enum Command {
     Exit,
     Echo { display_string: String },
-    NotFound,
+    Type { cmd_name: String },
+    NotFound { cmd_not_found: String },
 }
 
 impl Command {
     fn from_input(input: &str) -> Self {
-        let input = input.trim();
-        if input == "exit" {
-            return Self::Exit;
+        let trimmed = input.trim();
+
+        match trimmed {
+            "exit" => Command::Exit,
+            _ if trimmed.starts_with("echo ") => Command::Echo {
+                display_string: trimmed.strip_prefix("echo ").unwrap().to_string(),
+            },
+            _ if trimmed.starts_with("type ") => Command::Type {
+                cmd_name: trimmed.strip_prefix("type ").unwrap().to_string(),
+            },
+            _ => Command::NotFound {
+                cmd_not_found: trimmed.to_string(),
+            },
         }
-        if let Some(idx) = input.find("echo ") {
-            if idx == 0 {
-                return Self::Echo {
-                    display_string: input["echo ".len()..].to_string(),
-                };
-            }
-        }
-        Self::NotFound
     }
 }
 
@@ -36,7 +41,14 @@ fn main() {
         match command {
             Command::Exit => std::process::exit(0),
             Command::Echo { display_string } => println!("{}", display_string),
-            Command::NotFound => println!("{}: command not found", input.trim()),
+            Command::Type { cmd_name } => {
+                if BUILT_IN_COMMANDS.contains(&cmd_name.as_str()) {
+                    println!("{} is a shell builtin", cmd_name)
+                } else {
+                    println!("{}: command not found", cmd_name)
+                }
+            }
+            Command::NotFound { cmd_not_found } => println!("{}: command not found", cmd_not_found),
         }
     }
 }
